68832680c7ea221e98af8c4e4b86833c69660b7d
kXsato <cecile.hentsch.pro@gmail.com>
2026-03-15 11:02:58 +0100

    feat : installation de tinymce

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    EmilePerron\TinymceBundle\TinymceBundle::class => ['all' => true],
 ];
