5aba399b3fb5d9b01a5d9725c18f47e1476958e8
kXsato <cecile.hentsch.pro@gmail.com>
2026-03-15 10:21:27 +0100

    feat : ajouts du bundle tailwind et de la librairie de composants daisyui

--- importmap.php
+++ importmap.php
@@ -25,4 +25,11 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'daisyui' => [
+        'version' => '5.5.19',
+    ],
+    'daisyui/daisyui.min.css' => [
+        'version' => '5.5.19',
+        'type' => 'css',
+    ],
 ];
