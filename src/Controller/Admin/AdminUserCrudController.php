ebc3529e4a36820d397ecd5aa610e45a80b1cca5
kXsato <cecile.hentsch.pro@gmail.com>
2026-03-19 11:46:13 +0100

    feat: ajout de fixture user et correction sur le panel admin

--- src/Controller/Admin/AdminUserCrudController.php
+++ src/Controller/Admin/AdminUserCrudController.php
@@ -4,7 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -29,6 +34,16 @@ class AdminUserCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $qb = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+        $qb->andWhere('entity.id != :currentUser')
+            ->setParameter('currentUser', $this->getUser()->getId());
+
+        return $qb;
     }
 
     public function configureFields(string $pageName): iterable
