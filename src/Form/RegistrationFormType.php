b24bc4ea10ed0d1ce0b64b6b1c49316d1bd5de2c
kXsato <cecile.hentsch.pro@gmail.com>
2026-03-23 15:00:26 +0100

    feat: ajout d'e la coche CGU obligatoire

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,12 +5,14 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
@@ -53,6 +55,13 @@ class RegistrationFormType extends AbstractType
                         pattern: '/[0-9]/',
                         message: 'Le mot de passe doit contenir au moins un chiffre',
                     ),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label'    => false,
+                'mapped'   => false,
+                'constraints' => [
+                    new IsTrue(message: 'Vous devez accepter les CGU et la politique de confidentialité.'),
                 ],
             ])
         ;
