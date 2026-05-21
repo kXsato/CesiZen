b1cf86fd0704fa8acbcb8e924c9b0dd2eb080a07
kXsato <cecile.hentsch.pro@gmail.com>
2026-05-21 10:30:30 +0200

    hotfix: suppression du commentaire TODO dans RegistrationControllerTest (php:S1186)

--- tests/RegistrationControllerTest.php
+++ tests/RegistrationControllerTest.php
@@ -43,8 +43,6 @@ class RegistrationControllerTest extends WebTestCase
             'registration_form[agreeTerms]' => true,
         ]);
 
-        // Ensure the response redirects after submitting the form, the user exists, and is not verified
-        // self::assertResponseRedirects('/'); @TODO: set the appropriate path that the user is redirected to.
         self::assertCount(1, $this->userRepository->findAll());
     }
 }
